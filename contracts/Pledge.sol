b9d8b9336d12e389b5e1fb86acc471ed4093ee4a
gjacuna@gmail.com <gjacuna@gmail.com>
2021-10-15 20:58:50 -0300

    initial commit

---
+++ contracts/Pledge.sol
@@ -0,0 +1,18 @@
+// contracts/Pledge.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Pledge {
+    mapping(address => bool) public pledges;
+    mapping(address => uint256) public rates;
+
+    function pledge(uint256 _rate) public {
+        require(_rate>0, "Pledged rate has to be more than 0.");
+        pledges[msg.sender] = true;
+        rates[msg.sender] = _rate;
+    }
+
+    function isPledged(address _polluter) external view returns (bool) {
+        return pledges[_polluter]; //all values of a mapping are initialized with false
+    }
+}
